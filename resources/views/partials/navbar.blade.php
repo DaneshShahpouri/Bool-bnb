3c0817b4273d8850530e4090f672265118aa90f1
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-06-19 12:33:08 +0200

    fix: navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -18,7 +18,7 @@
                     <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:5173/">{{ __('Apartments') }}</a>
+                    <a class="nav-link" href="http://localhost:5173">{{ __('Apartments') }}</a>
                 </li>
             </ul>
 
