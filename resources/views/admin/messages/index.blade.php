a6810f02b2ef39b33716a94e951b659cb7268aac
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-06-27 15:52:49 +0200

    feat: added some styles to messages table

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -2,32 +2,35 @@
 
 @section('content')
 
-<table class="table table-light table-striped w-75 m-auto mt-5">
-    <thead>
-        <th>Apartment</th>
-        <th>Date</th>
-        <th>Username</th>
-        <th>Contet</th>
-        <th>Email</th>
-    </thead>
-
-    <tbody>
-        @foreach ($messages as $message)  
-        <tr>
-            <td>{{$message->apartment_id != null ? $message->apartment->name : 'Cancelled'}}</td>
-            <td>{{$message->created_at}}</td>
-            <td>{{$message->username}}</td>
-            <td>{{$message->content}}</td>
-            <td>{{$message->email}}</td>
-        </tr>
-        @endforeach
-    </tbody>
+<div id="messages_index">
+    <table class="table table-light table-striped w-75 m-auto mt-5">
+        <thead>
+            <th><i class="fa-solid fa-building"></i> Apartment</th>
+            <th><i class="fa-solid fa-clock"></i> Date</th>
+            <th><i class="fa-solid fa-user"></i> Username</th>
+            <th><i class="fa-solid fa-thumbtack"></i> Content</th>
+            <th><i class="fa-solid fa-envelope"></i> Email</th>
+        </thead>
     
-</table>
-
-<div class="m-4 d-flex justify-content-center">
-    <a class="btn btn-outline-primary" href="{{route('admin.apartments.index')}}">See your apartments</a>
+        <tbody>
+            @foreach ($messages as $message)  
+            <tr>
+                <td>{{$message->apartment_id != null ? $message->apartment->name : 'Cancelled'}}</td>
+                <td>{{$message->created_at}}</td>
+                <td>{{$message->username}}</td>
+                <td>{{$message->content}}</td>
+                <td>{{$message->email}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+        
+    </table>
+    
+    <div class="m-4 d-flex justify-content-center">
+        <a class="btn btn-outline-primary" href="{{route('admin.apartments.index')}}">See your apartments</a>
+    </div>
 </div>
+
 
 
 @endsection
